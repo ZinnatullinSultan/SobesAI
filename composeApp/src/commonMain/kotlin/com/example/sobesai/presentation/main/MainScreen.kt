602d33047884881bb1f4d80feec70e0dd2ebb921
ZinnatullinSultan <zinnatullin.969@gmail.com>
2026-04-05 23:30:12 +0300

    A few new things

--- composeApp/src/commonMain/kotlin/com/example/sobesai/presentation/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/sobesai/presentation/main/MainScreen.kt
@@ -1,8 +1,13 @@
 package com.example.sobesai.presentation.main
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -65,9 +70,12 @@ private fun MainScreenContent(
             SearchTopBar(
                 query = searchQuery,
                 onQueryChange = onSearchQueryChange,
-                onProfileClick = onProfileClick
+                onProfileClick = onProfileClick,
+                modifier = Modifier.windowInsetsPadding(
+                    WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal + WindowInsetsSides.Top)
+                )
             )
-        }
+        },
     ) { innerPadding ->
         PullRefreshWrapper(
             isRefreshing = isRefreshing,
